146fa9b58b6dd057e79fc16e4dcc01cdf82d5028
Mona Mohamed <42801687+monaelhoby@users.noreply.github.com>
2020-06-26 17:14:42 +0200

    Update user.js

--- src/modal/user.js
+++ src/modal/user.js
@@ -1,6 +1,6 @@
 class UserData {
     constructor (id, userName, photo, ownerId) {
-        this.id = id,
+        this.id = id;
         this.userName = userName;
         this.photo = photo;
         this.ownerId = ownerId
